9ef2c24d31c43d8a55d749e615a8cc89280fdb86
Bria White <bria.awhite@gmail.com>
2020-08-05 00:38:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,4 +3,8 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length > 2  
      array.insert(-2, "and")
-     array[0..]
+     array[0..-1].join(" , ")
+  else 
+    array.join 
+  end  
+end  
