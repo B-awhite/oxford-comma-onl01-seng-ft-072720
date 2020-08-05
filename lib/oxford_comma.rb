21cf49161b0dc62b60e779b4d5dd66ae4a60ad04
Bria White <bria.awhite@gmail.com>
2020-08-05 00:55:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
 end  
 
 def oxford_comma(array)
-  return array.join('and ') if array.length < 3
+  return array.join(' and ') if array.length < 3
   array[-1] = "and " + array[-1]
-  array.join(' , ')
+  array.join(', ')
 end  
